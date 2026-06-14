72c2943ebf913d26f720291e26b212f0a95b7bda
Marlon Braga <rdg.brg@gmail.com>
2026-06-14 13:05:53 -0500

    fix: replace RestTemplate with WebClient in IPApiService

--- src/main/java/com/clienthub/gateway/ipapi/IPApiService.java
+++ src/main/java/com/clienthub/gateway/ipapi/IPApiService.java
@@ -4,23 +4,27 @@ import com.clienthub.gateway.exception.custom.IPApiException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class IPApiService {
 
-    private final RestTemplate restTemplate;
+    private final WebClient.Builder webClientBuilder;
 
     public IPApiResponse ipAPICall(String userIp) {
         String apiUrl = "http://ip-api.com/json/" + userIp;
 
         try {
-            IPApiResponse apiResponse = restTemplate
-                    .getForObject(apiUrl, IPApiResponse.class);
+            IPApiResponse apiResponse = webClientBuilder.build()
+                    .get()
+                    .uri(apiUrl)
+                    .retrieve()
+                    .bodyToMono(IPApiResponse.class)
+                    .block();
 
-            log.info("Received API :: {}", apiResponse.toString());
+            log.info("RECEIVE API :: [{}]", apiResponse.toString());
             return apiResponse;
         } catch (Exception error) {
             log.error("IP API Request Error :: {}", error.getMessage(), error);
